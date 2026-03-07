ad53786d9ce8a633b05c4676580188214aaa57e9
QX_hao <1039757986@qq.com>
2026-03-07 21:20:49 +0800

    搭建基本框架

---
+++ internal/routers/router.go
@@ -0,0 +1,14 @@
+package routers
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.Use(cors.Config{})
+
+	return r
+}
